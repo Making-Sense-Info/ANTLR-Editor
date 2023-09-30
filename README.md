16e8f7f0569bf393a439bf1bb17dd6851ff60ed9
NicoLaval <NicoLaval@users.noreply.github.com>
2023-09-30 05:53:55 +0000

    deploy: 9e8751e9128624cef4d57f11708617c10bac7e76

--- README.md
+++
@@ -1,3 +0,0 @@
-# ANTLR Editor
-
-ANTLR Typescript editor.
--- src/stories/Editor.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Editor = ({ label, color }) => <div style={{ color }}>{label}</div>;
-
-export default Editor;
