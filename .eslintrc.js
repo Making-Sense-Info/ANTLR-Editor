46eb1227c1c0f2ee52988e5271d3228965202838
NicoLaval <nicolas.laval@making-sense.info>
2023-10-06 15:49:55 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,8 @@ module.exports = {
     "rules": {
         "no-extra-boolean-cast": "off",
         "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/ban-ts-comment": "off",
+        "no-debugger":"off"
     },
 };
